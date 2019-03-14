6e19c670fba6f953a23599d3094ff4a97cbfc3de
jaimefuentesp <jaimefuentesp@outlook.com>
2019-03-13 19:49:04 -0500

    FAB BUtton

--- app/src/main/java/com/jaimefuentesp/petjames2/MainActivity.java
+++ app/src/main/java/com/jaimefuentesp/petjames2/MainActivity.java
@@ -1,7 +1,9 @@
 package com.jaimefuentesp.petjames2;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        agregarFAB();
+    }
+
+    public void agregarFAB() {
+        FloatingActionButton miFAB = findViewById(R.id.fqbMiFab);
+        miFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
