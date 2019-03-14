2fc9d1fc25e97c1b8e5c880db27bb9779b146874
jaimefuentesp <jaimefuentesp@outlook.com>
2019-03-14 07:59:30 -0500

    Refreshing indicator

--- app/src/main/java/com/jaimefuentesp/petjames2/MainActivity.java
+++ app/src/main/java/com/jaimefuentesp/petjames2/MainActivity.java
@@ -2,19 +2,46 @@ package com.jaimefuentesp.petjames2;
 
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    SwipeRefreshLayout sfiMiIndicadorRefresh;
+    ListView lstMiLista;
+    Adapter adaptador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         agregarFAB();
+
+        sfiMiIndicadorRefresh = (SwipeRefreshLayout) findViewById(R.id.sfiMiIndicadorRefresh);
+        lstMiLista = (ListView) findViewById(R.id.lstMiLista);
+
+        String[] planetas=getResources().getStringArray(R.array.planetas);
+        lstMiLista.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1,planetas));
+        sfiMiIndicadorRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+            }
+        });
+
+    }
+
+    public void refrescandoContenido () {//volvemos a llenar la lista
+        String[] planetas=getResources().getStringArray(R.array.planetas);
+        lstMiLista.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1,planetas));
+        sfiMiIndicadorRefresh.setRefreshing(false);
     }
 
     public void agregarFAB() {
