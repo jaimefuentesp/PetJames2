b37a77f0c206283c8531cc72e25ae086b130c668
jaimefuentesp <jaimefuentesp@outlook.com>
2019-03-14 00:45:32 -0500

    FAB BUtton

--- app/src/main/java/com/jaimefuentesp/petjames2/MainActivity.java
+++ app/src/main/java/com/jaimefuentesp/petjames2/MainActivity.java
@@ -1,9 +1,12 @@
 package com.jaimefuentesp.petjames2;
 
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +22,17 @@ public class MainActivity extends AppCompatActivity {
         miFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                //Toast.makeText(getBaseContext(),getResources().getString(R.string.texto_FAVButton), Toast.LENGTH_SHORT).show();
+                //Snackbar.make(v,getResources().getString(R.string.texto_FAVButton),Snackbar.LENGTH_SHORT).show();
+                Snackbar.make(v,getResources().getString(R.string.texto_FAVButton),Snackbar.LENGTH_SHORT)
+                        .setAction(getResources().getString(R.string.texto_accion), new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Log.i("Snackbar","Click en snack bar");
+                            }
+                        })
+                        .setActionTextColor(getResources().getColor(R.color.colorPrimary))
+                        .show();
             }
         });
     }
